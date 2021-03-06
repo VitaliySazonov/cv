d7c196d9657263bed4eb125a4355e9787894a247
Vito <topvetal@gmail.com>
2021-03-06 20:59:41 +0200

    Add hbs

--- index.js
+++ index.js
@@ -1,7 +1,50 @@
-let express = require('express')
-let app = express()
-let path = require('path')
-let PORT = process.env.PORT || 5000
+let
+  express = require('express'),
+  app = express(),
+  path = require('path'),
+  PORT = process.env.PORT || 5000,
+  exphbs = require("express-handlebars"),
+  hbs= exphbs.create({
+    defaultLayout: "main",
+    extname: "hbs",
+  }),
+  pages = require('./public/js/pages')
+
+
+app.engine("hbs", hbs.engine);
+app.set("view engine", "hbs");
+app.set("views", "views");
+
+app.get("/", (req, res) => {
+  res.render("./pages/index", {
+    isHome: true,
+    title: 'CV',
+  })
+})
+
+app.get("/about", (req, res) => {
+  res.render("./pages/about", {
+    isAbout: true,
+    title: pages.about.title,
+    page: pages.about
+  })
+})
+
+app.get("/portfolio", (req, res) => {
+  res.render("./pages/portfolio", {
+    isPortfolio: true,
+    title: pages.portfolio.title,
+    page: pages.portfolio
+  })
+})
+
+app.get("/exp", (req, res) => {
+  res.render("./pages/exp", {
+    isExp: true,
+    title: pages.exp.title,
+    page: pages.exp
+  })
+})
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.listen(PORT, () => console.log(`PORT => ${PORT} has started...`))
---
+++ public/js/pages.js
@@ -0,0 +1,67 @@
+const pages = {
+  about: {
+    title: "About",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi aspernatur consectetur debitis dignissimos est facilis, fugiat ipsa nihil quaerat quasi ratione rem, soluta! Alias consequuntur eius eum fugiat nam obcaecati officiis porro. Doloribus error praesentium quam rerum sed!",
+    title_2: "Education",
+    description_2: [
+      `<li><p>2021</p><i></i><span><b>SkillUp</b> <br>Dnipro</span></li>`,
+      `<li><p>2019</p><i></i><span><b>IT Step Academy</b><br>Dnipro</span></li>`,
+      `<li><p>2018</p><i></i><span><b>BSc in Computer Graphics in the field of Computer Art Design</b><br>Dnipro</span></li>`,
+    ],
+    title_3: "Interests",
+    description_3: [
+      `<li><img src="img/hobbies/vel.png" alt=""><span>Cycling</span></li>`,
+      `<li><img src="img/hobbies/swim.png" alt=""><span>Swimming</span></li>`,
+      `<li><img src="img/hobbies/walk.png" alt=""><span>Walking</span></li>`,
+      `<li><img src="img/hobbies/read.png" alt=""><span>Reading</span></li>`,
+      `<li><img src="img/hobbies/music.png" alt=""><span>Music</span></li>`,
+      `<li><img src="img/hobbies/sci.png" alt=""><span>Science</span></li>`,
+      `<li><img src="img/hobbies/tea.png" alt=""><span>Tea</span></li>`,
+      `<li><img src="img/hobbies/cake.png" alt=""><span>Sweet</span></li>`,
+    ],
+  },
+  exp: {
+    title: "Experience",
+    description: [
+      `<li><p>Google</p><span>2021</span></li>`,
+      `<li><p>Facebook</p><span>2020</span></li>`,
+      `<li><p>Epam</p><span>2018</span></li>`,
+      `<li><p>Wix</p><span>2015</span></li>`
+    ],
+    title_2: 'Skills',
+    description_2: [
+      `<li><img src="img/skills/html5.png" alt=""><p>HTML5</p> <span>2 years</span></li>`,
+      `<li><img src="img/skills/css3.png" alt=""><p>CSS3</p> <span>2 years</span></li>`,
+      `<li><img src="img/skills/js.png" alt=""><p>JS</p> <span>2 years</span></li>`,
+      `<li><img src="img/skills/jq.png" alt=""><p>jQuery</p> <span>2 years</span></li>`,
+      `<li><img src="img/skills/bootstrap.png" alt=""><p>Bootstrap</p> <span>2 years</span></li>`,
+      `<li><img src="img/skills/react.png" alt=""><p>React</p> <span>2 months</span></li>`,
+      `<li><img src="img/skills/redux.png" alt=""><p>Redux</p> <span>2 months</span></li>`,
+      `<li><img src="img/skills/nextjs.png" alt=""><p>NextJS</p> <span>2 months</span></li>`,
+      `<li><img src="img/skills/nodejs.png" alt=""><p>NodeJS</p> <span>1 year</span></li>`,
+      `<li><img src="img/skills/nodejs.png" alt=""><p>ExpressJS</p> <span>1 year</span></li>`,
+      `<li><img src="img/skills/deno.png" alt=""><p>Deno</p> <span>1 year</span></li>`,
+      `<li><img src="img/skills/webpack.png" alt=""><p>Webpack</p> <span>1 year</span></li>`,
+      `<li><img src="img/skills/scss.png" alt=""><p>SCSS</p> <span>1 year</span></li>`,
+      `<li><img src="img/skills/sass.png" alt=""><p>SASS</p> <span>1 year</span></li>`,
+      `<li><img src="img/skills/sql.png" alt=""><p>MySQL</p> <span>1 year</span></li>`
+    ]
+  },
+  portfolio: {
+    title: 'Portfolio',
+    description: [
+      `<li class="portfolio_item"><a href="https://dribbble.com/" target="_blank"><img src="https://icons.iconarchive.com/icons/uiconstock/socialmedia/256/Dribbble-icon.png" alt=""><p>dribbble.com</p></a></li>`,
+      `<li class="portfolio_item"><a href="https://playcode.io/" target="_blank"><img src="https://playcode.io/lstatic/img/logo.png" alt=""><p>playcode.io</p></a></li>`,
+      `<li class="portfolio_item"><a href="https://quizlet.com/" target="_blank"><img src="https://assets.quizlet.com/a/j/dist/i/favicon.6e263725c926227.ico" alt=""><p>quizlet.com</p></a></li>`,
+      `<li class="portfolio_item"><a href="http://seasonvar.ru/" target="_blank"><img src="http://cdn.seasonvar.ru/images/fav/favicon.ico" alt=""><p>seasonvar.ru</p></a></li>`,
+      `<li class="portfolio_item"><a href="https://css-tricks.com/" target="_blank"><img src="https://css-tricks.com/favicon.ico" alt=""><p>css-tricks.com</p></a></li>`,
+      `<li class="portfolio_item"><a href="https://materializecss.com/" target="_blank"><img src="https://materializecss.com/images/favicon/apple-touch-icon-152x152.png" alt=""><p>materializecss.com</p></a></li>`,
+      `<li class="portfolio_item"><a href="https://www.protopie.io/" target="_blank"><img src="https://img.apksum.com/28/io.protopie.companion/5.1.0/icon.png" alt=""><p>protopie.io</p></a></li>`,
+      `<li class="portfolio_item"><a href="https://www.w3schools.com/" target="_blank"><img src="https://www.w3schools.com/favicon.ico" alt=""><p>w3schools.com</p></a></li>`,
+      `<li class="portfolio_item"><a href="https://www.pexels.com/" target="_blank"><img src="https://www.pexels.com/favicon.ico" alt=""><p>pexels.com</p></a></li>`
+    ]
+  }
+}
+
+module.exports = pages;
